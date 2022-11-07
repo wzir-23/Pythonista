7c508120a09f0cbeadfce61c96ab5f5760b9e1ab
wzir-23 <wzir@borken.se>
2022-11-07 23:10:20 +0100

    Not working. Tried to add gui

--- FastFasting/FastFasting.py
+++ FastFasting/FastFasting.py
@@ -3,12 +3,17 @@ import os
 import sqlite3
 import ui
 
+db_connection = ''
+cursor = ''
 
 
+@ui.in_background
 def ToggleFasting(sender):
     ''' The start/stop button was pressed '''
+    global db_connection
+    global cursor
     started = get_status(cursor)
-    if fasting_started:
+    if started:
         sender.title = 'Start fasting'
     else:
         sender.title = 'Stop fasting'
@@ -21,17 +26,18 @@ def RefreshPressed(sender):
 
 
 def connect_db(fname):
-    """ connect to sqlite3 db or create if missing """
+    ''' connect to sqlite3 db or create if missing '''
+    global db_connection
+    global cursor
     create_tables = False
-    if not os.path.isfile(fname): # no such file
+    if not os.path.isfile(fname):  # no such file
         create_tables = True
-    db_connection = sqlite3.connect(fname)
+    db_connection = sqlite3.connect(fname)  #, check_same_thread=False)
     cursor = db_connection.cursor()
     if create_tables:
         cursor.execute('''CREATE TABLE fasting(start_time text,
                           stop_time text)''')
-    return db_connection, cursor
-        
+
 
 def list_database(cursor):
     ''' list all database intries '''
@@ -41,12 +47,14 @@ def list_database(cursor):
 
 
 def last_db_entry(cursor):
+    ''' return rowid + data from last db insert '''
     sql = 'SELECT rowid,* FROM fasting ORDER BY start_time DESC LIMIT 1'
     cursor.execute(sql)
     return cursor.fetchall()
 
 
 def get_status(cursor):
+    ''' determine if a period is "open" '''
     last_entry = last_db_entry(cursor)
     if not last_entry:   # db empty
         return False
@@ -71,13 +79,11 @@ def add_time_to_db(db_connection, cursor, started):
         db_connection.commit()
 
 
-                        
-        
-# The loading must appear after defining the ui actions
-
 def main():
+    ''' database initialization before loading gui ''' 
     fname = 'FastFasting.db'
-    db_connection, cursor = connect_db(fname)
+    connect_db(fname)
+    # The load must appear after defining the ui actions
     v = ui.load_view('FastFasting')
     v.present('sheet')
 
